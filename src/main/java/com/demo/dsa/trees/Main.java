c5b710c914917c1c4cd90dcb010c29fe0edf6362
tejakathari007 <tejakathari007@gmail.com>
2022-02-17 22:50:24 +0530

    Added BinaryTree using LinkedList

--- src/main/java/com/demo/dsa/trees/Main.java
+++ src/main/java/com/demo/dsa/trees/Main.java
@@ -4,21 +4,58 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		TreeNode drinks = new TreeNode("Drinks");
-		TreeNode hot = new TreeNode("hold");
-		TreeNode tea = new TreeNode("tea");
-		TreeNode coffee = new TreeNode("coffee");
-		TreeNode cold = new TreeNode("cold");
-		TreeNode lemonade = new TreeNode("lemonade");
-		TreeNode beer = new TreeNode("beer");
-		cold.addChild(beer);
-		cold.addChild(lemonade);
-		hot.addChild(coffee);
-		hot.addChild(tea);
-		drinks.addChild(cold);
-		drinks.addChild(hot);
+//		TreeNode drinks = new TreeNode("Drinks");
+//		TreeNode hot = new TreeNode("hold");
+//		TreeNode tea = new TreeNode("tea");
+//		TreeNode coffee = new TreeNode("coffee");
+//		TreeNode cold = new TreeNode("cold");
+//		TreeNode lemonade = new TreeNode("lemonade");
+//		TreeNode beer = new TreeNode("beer");
+//		cold.addChild(beer);
+//		cold.addChild(lemonade);
+//		hot.addChild(coffee);
+//		hot.addChild(tea);
+//		drinks.addChild(cold);
+//		drinks.addChild(hot);
+//
+//		System.out.println(drinks.print(0));
 
-		System.out.println(drinks.print(0));
+//		BinaryTreeLinkedList binaryTree = new BinaryTreeLinkedList();
+
+//		BinaryNode N6 = new BinaryNode("N6");
+//		BinaryNode N7 = new BinaryNode("N7");
+//		BinaryNode N8 = new BinaryNode("N8");
+//		BinaryNode N9 = new BinaryNode("N9");
+//		BinaryNode N4 = new BinaryNode("N4", N8, N9);
+//		BinaryNode N5 = new BinaryNode("N5");
+//		BinaryNode N2 = new BinaryNode("N2", N4, N5);
+//		BinaryNode N3 = new BinaryNode("N3", N6, N7);
+//		BinaryNode N1 = new BinaryNode("N1", N2, N3);
+
+//		binaryTree.preOrderTraversal(N1);
+//		binaryTree.inOrderTraversal(N1);
+//		binaryTree.postOrderTraversal(N1);
+
+//		binaryTree.root=N1;
+//		binaryTree.levelOrderTraversal();
+
+//		binaryTree.root=N1;
+//		binaryTree.search("N15");
+//
+//		binaryTree.insert("N1");
+//		binaryTree.insert("N2");
+//		binaryTree.insert("N3");
+//		binaryTree.insert("N4");
+//		binaryTree.insert("N5");
+//		binaryTree.insert("N6");
+//		binaryTree.insert("N7");
+//		binaryTree.insert("N8");
+//		binaryTree.insert("N9");
+//		System.out.println();
+
+//		binaryTree.deleteNode("N4");
+//		binaryTree.levelOrderTraversal();
+
 
 	}
 
